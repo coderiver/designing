b6f56df93023c0812f97f0d77edf294c656f3f6c
etna-scorpio <etna.scorpio@gmail.com>
2015-08-07 16:26:57 +0300

    done

--- js/common.js
+++ js/common.js
@@ -1,12 +1,62 @@
 head.ready(function() {
 
-	// $(document).on("click", function(){
-	// 	$(".js-popup").hide();
-	// });
-
+	//menu dropdown
 	$('#btn-menu').click(function(){
 		$('.js-nav').toggleClass('is-active');
 	});
 
+	//scrollTo
+	function scrollTo(page){
+
+		$('html, body').animate({
+			scrollTop: $(page).offset().top
+		}, 800);
+
+	}
+
+	$('.js-down').click(function (e) {
+
+		e.preventDefault();
+		var page = $(this).attr("href");
+		scrollTo(page);
+
+	});
+
+	//rotator
+	(function() {
+
+		var rotator 	= $('.js-type');
+		var authorBl 	= $('.js-author');
+		var avatar 		= $('.js-author').find('img');
+		var nick		= $('.js-author').find('span');
+		var authors 	= [
+			{avatar: 'img/icons/avatar3.png', nick: '4vida.ru'},
+			{avatar: 'img/icons/avatar2.png', nick: 'elenakrygina.com'},
+			{avatar: 'img/icons/avatar.png', nick: '@stolyarovyuriy'}
+		]
+
+		$.each(authors, function(index, author) {
+            $('<img />').attr('src', author.avatar);
+        });
+
+		rotator.typed({
+			strings: ['safe', 'accurate', 'secure'],
+			typeSpeed: 100,
+			backSpeed: 50,
+			backDelay: 4000,
+			loop: true,
+			showCursor: false,
+			preStringTyped: function(){
+				authorBl.removeClass('is-active');
+			},
+			onStringTyped: function(index){
+				avatar.attr('src', authors[index].avatar);
+				nick.text(authors[index].nick);
+				authorBl.addClass('is-active');
+			},
+			contentType: 'text'
+		});
+
+	})();
 
 });
